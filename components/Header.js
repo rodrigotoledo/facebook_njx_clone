0f860d18d1a0c71c4625dc062d9946cdbe0fef4d
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2021-10-20 06:32:00 -0300

    header with styles

--- components/Header.js
+++ components/Header.js
@@ -13,24 +13,45 @@ import {
   SearchIcon,
   ShoppingCartIcon
 } from "@heroicons/react/outline";
+import HeaderIcon from './HeaderIcon';
 
 
 export default function Header() {
   return (
-    <div>
+    <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
 
-      {/* left */}
+      {/* left section */}
       <div className='flex items-center'>
         <Image src="https://links.papareact.com/5me" width={40} height={40} layout='fixed' />
 
         <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
           <SearchIcon className='h-6 text-gray-600' />
-          <input type='text' placeholder="Search Facebook" className='flex ml-2 items-center bg-transparent outline-none' />
+          <input type='text' placeholder="Search Facebook" className='hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' />
         </div>
       </div>
 
-      {/* center */}
-      {/* right */}
+      {/* center section */}
+      <div className='flex justify-center flex-grow'>
+        <div className='flex space-x-6 md:space-x-2'>
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+
+      {/* right section */}
+
+      <div className='flex items-center sm:space-x-2 justify-end'>
+        {/* profile pic */}
+        <p className='font-semibold pr-3 whitespace-nowrap'>Rodrigo Toledo</p>
+        <ViewGridIcon className='icon' />
+        <ChatIcon className='icon' />
+        <BellIcon className='icon' />
+        <ChevronDownIcon className='icon' />
+      </div>
+
     </div>
   )
 }
